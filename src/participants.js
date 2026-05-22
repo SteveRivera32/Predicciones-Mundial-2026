5c14e3a94cf31a73eeb6dc7e4a65727f0a97b311
SteveRivera32 <steve.rivera32@unitec.edu>
2026-05-22 17:56:20 -0600

    contraseñas cambiadas

--- src/participants.js
+++ src/participants.js
@@ -12,14 +12,14 @@ import { pushParticipants } from "./sync-push.js";
 import { clearPinVerifiedForParticipant } from "./session.js";
 
 export const BUILTIN_PARTICIPANTS = [
-  { id: "tivo", name: "Tivo", pin: "xd12" },
+  { id: "tivo", name: "Tivo", pin: "xd1278" },
   { id: "admin", name: "ADMIN", pin: "503965" },
-  { id: "rick", name: "Rick", pin: "null" },
-  { id: "ozeb", name: "Ozeb", pin: null },
-  { id: "elcalvo", name: "ElCalvo", pin: null },
-  { id: "akinian", name: "Akinian", pin: null },
-  { id: "ale", name: "Ale", pin: null },
-  { id: "jonny", name: "Jonny", pin: null },
+  { id: "rick", name: "Rick", pin: "cr01392" },
+  { id: "ozeb", name: "Ozeb", pin: "pilin7891" },
+  { id: "elcalvo", name: "ElCalvo", pin: "sexoh741" },
+  { id: "akinian", name: "Akinian", pin: "pene99391" },
+  { id: "ale", name: "Ale", pin: "jijimmd237" },
+  { id: "jonny", name: "Jonny", pin: "culo200104" },
 ];
 
 /**
